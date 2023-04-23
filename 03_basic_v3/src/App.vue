745a9291a14164b95306ae0827c39e9d9b43b7d0
Yuta Okuma <y.dream.nest@icloud.com>
2023-04-23 10:32:30 +0900

    feat: :sparkles: 条件分岐

--- 03_basic_v3/src/App.vue
+++ 03_basic_v3/src/App.vue
@@ -1,19 +1,29 @@
 <script setup>
-const activeColor = 'red';
-const fontStress = '900';
-
-const styleObject = {
-  color: 'red',
-  fontWeight: 900,
-};
-
-// const styleObject = 'color:red;font-weight:900';
+// const error = true;
+const error = '';
+const stock = 0;
+const error2 = true;
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <!-- <p :style="{ color: activeColor, fontWeight: fontStress }">v-bindの設定方法の確認</p> -->
-  <p :style="styleObject">v-bindの設定方法の確認</p>
+  <div v-if="error">{{ error }}</div>
+  <div v-else>エラーはありません。</div>
+  <!-- <div v-if="error">エラーが発生しています。</div> -->
+
+  <div v-if="stock > 5">まだ商品の在庫数に余裕があります。</div>
+  <div v-else-if="stock === 0">申し訳ございません。現在売り切れです。</div>
+  <div v-else>在庫数が少なくなっています。お急ぎください。</div>
+
+  <div v-if="error2">エラーが発生しています。(v-if)</div>
+  <div v-show="error2">エラーが発生しています。(v-show)</div>
+  <!--
+  v-if、v-showの違い
+  条件がfalseの場合
+  v-showのdisplayのnone、blockの切り替えはv-ifによる要素の追加、削除より処理の負荷が低い。
+  ユーザーとのインタラクティブによってスピードは重要なので、可能な限り負荷が低い処理を実行することに意味がある。
+  例えばドロップダウンメニューのように非表示の時には何も表示されず、表示・非表示を繰り返すような機能にv-showは適している。
+  -->
 </template>
 
 <style></style>
--- 03_basic_v3/src/main.js
+++ 03_basic_v3/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import './style.css';
 // import App from './App01.vue';
 // import App from './App02.vue';
+// import App from './App03.vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
