3f34276c6a9f4a09470367ee90bacb9c13ddb2b7
Yuta Okuma <y.dream.nest@icloud.com>
2023-04-23 09:58:25 +0900

    feat: :sparkles: Binding（バインディング）- style属性の設定

--- 03_basic_v3/src/App.vue
+++ 03_basic_v3/src/App.vue
@@ -1,41 +1,19 @@
 <script setup>
-const link = 'https://google.com';
-const isActive = true;
-const isWhite = true;
+const activeColor = 'red';
+const fontStress = '900';
+
+const styleObject = {
+  color: 'red',
+  fontWeight: 900,
+};
+
+// const styleObject = 'color:red;font-weight:900';
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <div>
-    <a v-bind:href="link">Google</a>
-    <!-- 
-    v-bindを頻繁に利用する例
-    ・imgタグのsrc属性
-    ・buttonタグのdisabled属性
-    -->
-
-    <!-- class属性 -->
-    <p class="active">v-bindの設定方法の確認</p>
-    <p class="underline" :class="{ active: isActive }">v-bindの設定方法の確認</p>
-    <p :class="{ 'underline active': isActive }">v-bindの設定方法の確認</p>
-    <!-- 複数のclassを指定する -->
-    <p class="underline" :class="{ active: isActive, back: isWhite }">v-bindの設定方法の確認</p>
-    <p :class="{ 'underline active': isActive, back: isWhite }">v-bindの設定方法の確認</p>
-    <!-- 論理演算子を使ったclassを指定する -->
-    <p :class="isActive && 'active'">v-bindの設定方法の確認</p>
-    <p :class="isActive ? 'underline' : 'active'">v-bindの設定方法の確認</p>
-  </div>
+  <!-- <p :style="{ color: activeColor, fontWeight: fontStress }">v-bindの設定方法の確認</p> -->
+  <p :style="styleObject">v-bindの設定方法の確認</p>
 </template>
 
-<style>
-.active {
-  color: red;
-  font-weight: 900;
-}
-.underline {
-  text-decoration: underline;
-}
-.back {
-  background-color: white;
-}
-</style>
+<style></style>
