00d35dd01376cde41da65109bed62520620f87c7
Yuta Okuma <y.dream.nest@icloud.com>
2023-04-28 09:04:13 +0900

    feat: :sparkles: Dynamicコンポーネント

--- 04_components_v3/src/App.vue
+++ 04_components_v3/src/App.vue
@@ -1,26 +1,31 @@
 <script setup>
-import User from './components/User2.vue';
-import User2 from './components/User3.vue';
+import { ref, computed } from 'vue';
+import Tokyo from './components/Tokyo.vue';
+import Kyoto from './components/Kyoto.vue';
+
+const city = ref('tokyo');
+
+const tabs = {
+  tokyo: Tokyo,
+  kyoto: Kyoto,
+};
+const tab = computed(() => tabs[city.value]);
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <User>
-    <!-- defaultのslotからslot propsを受け取るので、v-slot:defaultに任意の名前を設定する -->
-    <!-- 分割代入でpropsの値を受け取ることもできる -->
-    <template v-slot:default="{ message, content }"> {{ message }}/{{ content }} </template>
-    <!-- v-slotを#にして省略できる -->
-    <template #header="slotProps">
-      <div>{{ slotProps }}</div>
-    </template>
-  </User>
-  <ul>
-    <User2>
-      <!-- slot propsのuserを受け取った親コンポーネントでオブジェクトのプロパティを表示 -->
-      <!-- 親コンポーネントで表示内容を決定できるため、コンポーネントの使い所が増える -->
-      <template v-slot="{ user }">
-        <li>{{ user.name }}</li>
-      </template>
-    </User2>
-  </ul>
+  <div>
+    <button @click="city = 'tokyo'">東京</button>
+    <button @click="city = 'kyoto'">京都</button>
+  </div>
+  <div>
+    <!-- reactive変数とv-ifディレクティブを使って表示を切り替える -->
+    <Tokyo v-if="city === 'tokyo'" />
+    <Kyoto v-else />
+  </div>
+  <!-- componentタグとis属性を利用してDynamicコンポーネントを実装 -->
+  <!-- コンポーネントを切り替えた場合にコンポーネントの状態を保持するためにkeep-aliveタグで囲う -->
+  <keep-alive>
+    <component v-bind:is="tab"></component>
+  </keep-alive>
 </template>
--- 04_components_v3/src/main.js
+++ 04_components_v3/src/main.js
@@ -6,6 +6,7 @@ import './style.css';
 // import App from './App/App04.vue';
 // import App from './App/App05.vue';
 // import App from './App/App06.vue';
+// import App from './App/App07.vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
