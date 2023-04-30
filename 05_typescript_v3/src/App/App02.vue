c06477572294b1acee61ea6b04b1945e6d8f5bc3
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-01 03:01:17 +0900

    feat: :sparkles: 関数の型設定

--- 05_typescript_v3/src/App/App02.vue
+++ 05_typescript_v3/src/App/App02.vue
@@ -14,12 +14,6 @@ const user: User = reactive({
 });
 
 const fullName = computed<string>(() => `${user.firstName} ${user.lastName}`);
-
-const changeName = (name: string): void => {
-  user.firstName = name;
-};
-
-changeName('Jane');
 </script>
 
 <template>
--- 05_typescript_v3/src/main.ts
+++ 05_typescript_v3/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 // import App from './App/App01.vue'
+// import App from './App/App02.vue'
 import App from './App.vue';
 
 import './assets/main.css';
