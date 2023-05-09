663592316f72dad2b14656eae9eb28d5b459fd74
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-09 23:20:34 +0900

    feat: :sparkles: ドロップダウンメニューにダークモードを設定

--- 06_dashboard/src/components/DropdownMenu.vue
+++ 06_dashboard/src/components/DropdownMenu.vue
@@ -19,15 +19,19 @@ const toggle = () => {
       leave-active-class="transitionduration-300"
       leave-to-class="transform opacity-0 duration-300"
     >
-      <div class="absolute top-16 right-0 z-10 w-40 py-2 bg-white rounded-sm shadow" v-show="show">
+      <div class="absolute top-16 right-0 z-10 w-40 py-2 bg-white dark:bg-gray-800 rounded-sm shadow" v-show="show">
         <ul>
-          <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
+          <li
+            class="text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-600 p-2"
+          >
             <a href="/#" class="flex items-center space-x-2">
               <UserIcon class="w-5 h-5" />
               <span class="text-sm font-bold">プロファイル</span>
             </a>
           </li>
-          <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
+          <li
+            class="text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-600 p-2"
+          >
             <a href="/#" class="flex items-center space-x-2">
               <LogoutIcon class="w-5 h-5" />
               <span class="text-sm font-bold">ログアウト</span>
