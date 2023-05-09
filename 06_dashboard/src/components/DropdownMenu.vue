a4c9620af0ba6b28dacf750ccc4f686d3f90b619
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-09 23:14:21 +0900

    feat: :sparkles: ドロップダウンメニューのアニメーションを設定

--- 06_dashboard/src/components/DropdownMenu.vue
+++ 06_dashboard/src/components/DropdownMenu.vue
@@ -13,21 +13,28 @@ const toggle = () => {
 <template>
   <div class="relative">
     <img src="../assets/avatar.jpg" alt="avatar" class="rounded-full w-10 h-10 cursor-pointer" @click="toggle" />
-    <div class="absolute top-16 right-0 z-10 w-40 py-2 bg-white rounded-sm shadow" v-show="show">
-      <ul>
-        <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
-          <a href="/#" class="flex items-center space-x-2">
-            <UserIcon class="w-5 h-5" />
-            <span class="text-sm font-bold">プロファイル</span>
-          </a>
-        </li>
-        <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
-          <a href="/#" class="flex items-center space-x-2">
-            <LogoutIcon class="w-5 h-5" />
-            <span class="text-sm font-bold">ログアウト</span>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <transition
+      enter-active-class="transition duration-300"
+      enter-from-class="transform opacity-0 -translate-y-2 duration-300"
+      leave-active-class="transitionduration-300"
+      leave-to-class="transform opacity-0 duration-300"
+    >
+      <div class="absolute top-16 right-0 z-10 w-40 py-2 bg-white rounded-sm shadow" v-show="show">
+        <ul>
+          <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
+            <a href="/#" class="flex items-center space-x-2">
+              <UserIcon class="w-5 h-5" />
+              <span class="text-sm font-bold">プロファイル</span>
+            </a>
+          </li>
+          <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
+            <a href="/#" class="flex items-center space-x-2">
+              <LogoutIcon class="w-5 h-5" />
+              <span class="text-sm font-bold">ログアウト</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </div>
 </template>
