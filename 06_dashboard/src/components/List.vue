f5597121381ac82f49f1631e2cdfef10d2ce280e
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-11 08:00:32 +0900

    feat: :sparkles: Dynamic Componentsによるアイコン設定

--- 06_dashboard/src/components/List.vue
+++ 06_dashboard/src/components/List.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { reactive } from 'vue';
+import { RectangleGroupIcon as TemplateIcon, ShoppingCartIcon } from '@heroicons/vue/24/outline';
 
 // メニューリストの情報
 const lists = reactive([
@@ -24,12 +25,19 @@ const lists = reactive([
     ],
   },
 ]);
+
+// dynamic componentsを利用するためにアイコン名対応のオブジェクトを用意
+const icons = {
+  TemplateIcon: TemplateIcon,
+  ShoppingCartIcon: ShoppingCartIcon,
+};
 </script>
 
 <template>
   <ul class="text-gray-700">
     <li class="mb-1" v-for="list in lists" :key="list.name">
-      <a :href="list.link" class="block p-2 rounded-sm hover:text-white hover:bg-blue-400">
+      <a :href="list.link" class="flex items-center p-2 rounded-sm hover:text-white hover:bg-blue-400">
+        <component :is="icons[list.icon]" class="w-6 h-6 mr-2"></component>
         <span>{{ list.name }}</span>
       </a>
     </li>
