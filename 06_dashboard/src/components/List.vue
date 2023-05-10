a8f816508308fcb6ff3e2502f12a35e10602aa5f
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-11 07:47:37 +0900

    feat: :sparkles: メニューリストの設定

---
+++ 06_dashboard/src/components/List.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { reactive } from 'vue';
+
+// メニューリストの情報
+const lists = reactive([
+  {
+    name: 'ダッシュボード',
+    icon: 'TemplateIcon',
+    link: '/',
+  },
+  {
+    name: 'EC',
+    icon: 'ShoppingCartIcon',
+    link: '/#',
+    sublists: [
+      {
+        name: '商品一覧',
+        link: '/#',
+      },
+      {
+        name: '注文一覧',
+        link: '/#',
+      },
+    ],
+  },
+]);
+</script>
+
+<template>
+  <ul class="text-gray-700">
+    <li class="mb-1" v-for="list in lists" :key="list.name">
+      <a :href="list.link" class="block p-2 rounded-sm hover:text-white hover:bg-blue-400">
+        <span>{{ list.name }}</span>
+      </a>
+    </li>
+  </ul>
+</template>
