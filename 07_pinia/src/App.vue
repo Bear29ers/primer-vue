80ac07e8f71ce82bb5531ab645c9e10d70bcf9c5
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-13 08:11:29 +0900

    feat: :sparkles: 他コンポーネントからのアクセス

--- 07_pinia/src/App.vue
+++ 07_pinia/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { useStoreCounter } from './stores/counter';
+import HelloPinia from './components/HelloPinia.vue';
+
 const counter = useStoreCounter();
 </script>
 
 <template>
   <h1>Pinia入門</h1>
   <p>Count: {{ counter.count }}</p>
+  <HelloPinia />
 </template>
