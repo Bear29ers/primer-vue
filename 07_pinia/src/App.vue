f3249f31b4d02875b6700e415ba3c97ef5508411
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-13 08:34:27 +0900

    feat: :sparkles: 分割代入の設定

--- 07_pinia/src/App.vue
+++ 07_pinia/src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { useStoreCounter } from './stores/counter';
 import HelloPinia from './components/HelloPinia.vue';
+import { storeToRefs } from 'pinia';
 
 const counter = useStoreCounter();
+// 分割代入を行ってもreactiveな性質を保持するためにはstoreToRefs関数を利用する必要がある
+const { count } = storeToRefs(counter);
+const { increment } = counter;
 </script>
 
 <template>
   <h1>Pinia入門</h1>
-  <p>Count: {{ counter.count }}</p>
+  <p>Count: {{ count }}</p>
   <div>
-    <button @click="counter.increment">Up</button>
+    <button @click="increment">Up</button>
   </div>
   <HelloPinia />
 </template>
