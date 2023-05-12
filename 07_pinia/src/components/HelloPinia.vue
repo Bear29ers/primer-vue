ec6c447dbc4a6ee9667044749aba61b12a6864d7
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-13 08:59:24 +0900

    feat: :sparkles: $patchメソッド

--- 07_pinia/src/components/HelloPinia.vue
+++ 07_pinia/src/components/HelloPinia.vue
@@ -1,6 +1,23 @@
 <script setup>
 import { useStoreCounter } from '../stores/counter';
+
 const counter = useStoreCounter();
+// $patchメソッドでstateの値を更新
+const patch = () => {
+  counter.$patch({
+    count: 100,
+    user: {
+      name: 'Jane Doe',
+    },
+  });
+
+  /*
+  counter.$patch((state) => {
+    state.count = 10;
+    state.user.name = 'Kevin Doe';
+  });
+   */
+};
 </script>
 
 <template>
@@ -8,4 +25,5 @@ const counter = useStoreCounter();
   <p>カウント: {{ counter.count }}</p>
   <!-- $resetメソッドにより初期値にリセット -->
   <button @click="counter.$reset">Reset</button>
+  <button @click="patch">Patch</button>
 </template>
