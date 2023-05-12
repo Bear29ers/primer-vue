99710b8970693be0d229bb0adf1454bb239adaa0
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-13 08:07:21 +0900

    feat: :sparkles: Storeのデータへのアクセス確認

--- 07_pinia/src/stores/counter.js
+++ 07_pinia/src/stores/counter.js
@@ -3,5 +3,9 @@ import { defineStore } from 'pinia';
 export const useStoreCounter = defineStore('counter', {
   state: () => ({
     count: 1,
+    count2: 2,
+    user: {
+      name: 'John Doe',
+    },
   }),
 });
